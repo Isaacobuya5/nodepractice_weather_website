48dc72cb17de0d8addbc7274fdf4d83185bbeae5
Isaac Obuya <Szack4477@gmail.com>
2020-06-10 14:01:36 +0300

    render dynamic HTML templates using handlebars

--- src/app.js
+++ src/app.js
@@ -12,23 +12,57 @@
  * we pass the port number and optional callback arguement to the method above.
  * the node process continues running until we close the server
  */
-
+const path = require('path');
  const express = require('express');
  // we instantiate our app as an express application by invoking express function
  const app = express();
 
+ const publicDirectory = path.join(__dirname, '../public');
+
+ app.use(express.static(publicDirectory));
+
+ // allows you to set a value for express settings i.e. key, setting name and setting value
+ app.set('view engine', 'hbs');
+
+ // setting up route for the template
  app.get('', (req, res) => {
-     // res.send allows us to send something back to the requestor
-    res.send('Hello express!');
+     // render our view
+     res.render('index', {
+         title: "Dynamically render HTML using express",
+         name: "Isaac Obuya"
+     });
  });
 
- app.get('/help', (req, res) => {
-     res.send("Help page!")
- });
-
+ // set up route for about page
  app.get('/about', (req, res) => {
-     res.send("About page!")
+     // render about view
+    res.render('about', {
+        title: 'About page',
+        name: "Isaac Obuya"
+    });
  });
+
+ // set up route for help page
+ app.get('/help', (req, res) => {
+     res.render('help', {
+         message: "Contact system admin for help"
+     });
+ });
+
+//  app.get('/help', (req, res) => {
+//      res.send("Help page!")
+//  });
+
+//  app.get('/about', (req, res) => {
+//      res.send([{
+//          name: "Isaac Obuya",
+//          age: 24
+//      },
+//     {
+//         name: "THomas Tom",
+//         age: 36
+//     }])
+//  });
 
  app.get('/weather', (req, res) => {
      res.send("Weather forecast page");
